f58cbae87d58313cb6b0049446f1ff7d5386460c
johnn134 <johnn134@gmail.com>
2018-09-09 10:12:11 -0400

    Copy over template web app code

---
+++ src/js/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    render () {
+
+        return (
+            <div className="container">
+                <p>Web Application</p>
+            </div>
+        );
+
+    }
+
+}
+
+export default App;
